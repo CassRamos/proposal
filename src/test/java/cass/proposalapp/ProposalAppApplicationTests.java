2acce76ff6953e27eb52b107b954465bea10db2a
Cass <cassianoleaoramos@gmail.com>
2025-01-31 22:55:44 -0300

    Initial project configuration

---
+++ src/test/java/cass/proposalapp/ProposalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package cass.proposalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProposalAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
