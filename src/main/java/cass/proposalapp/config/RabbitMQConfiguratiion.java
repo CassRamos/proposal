7096c7cd704884233d35ce14831dea139eb6d1f9
Cass <cassianoleaoramos@gmail.com>
2025-02-07 21:30:19 -0300

    Add RabbitMQ connection parameters and configure queues and exchange

---
+++ src/main/java/cass/proposalapp/config/RabbitMQConfiguratiion.java
@@ -0,0 +1,68 @@
+package cass.proposalapp.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfiguratiion {
+
+    private ConnectionFactory connectionFactory;
+
+    public RabbitMQConfiguratiion(ConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
+
+    @Bean
+    public Queue createQueuePendingCreditAnalysisProposal() {
+        return QueueBuilder.durable("pending-proposal.credit-analysis").build();
+    }
+
+    @Bean
+    public Queue createQueuePendingProposalNotification() {
+        return QueueBuilder.durable("pending-proposal.notification").build();
+    }
+
+    @Bean
+    public Queue createQueueCompletedProposal() {
+        return QueueBuilder.durable("completed-proposal.proposal").build();
+    }
+
+    @Bean
+    public Queue createQueueCompletedProposalNotification() {
+        return QueueBuilder.durable("completed-proposal.notification").build();
+    }
+
+    @Bean
+    public RabbitAdmin createRabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> initializeAdmin(RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
+    }
+
+    @Bean
+    public FanoutExchange createFanoutExchange() {
+        return new FanoutExchange("pending-proposal.exchange");
+    }
+
+    @Bean
+    public Binding createPendingCreditAnalysisBinding() {
+        return BindingBuilder
+                .bind(createQueuePendingCreditAnalysisProposal())
+                .to(createFanoutExchange());
+    }
+
+    @Bean
+    public Binding createPendingProposalNotificationBinding() {
+        return BindingBuilder
+                .bind(createQueuePendingProposalNotification())
+                .to(createFanoutExchange());
+    }
+}
